47c83420101eba233869e22221d456c375705447
sunpeng93 <sunpeng93@jd.com>
2020-07-08 18:28:28 +0800

    demo

--- src/main/java/com/example/abstracttest/PersonRunC.java
+++ src/main/java/com/example/abstracttest/PersonRunC.java
@@ -1,5 +1,6 @@
 package com.example.abstracttest;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 /**
@@ -9,9 +10,11 @@ import org.springframework.stereotype.Service;
  * @Date 2020/7/8
  */
 @Service("personRunc")
+@Slf4j
 public class PersonRunC implements RunC{
     @Override
     public void runT(){
         System.out.println("this is person run");
+        log.info("this is person run");
     }
 }
