fb3ccacdb0eff2dcbaac4bb2cf6042c91d95012d
sunpeng93 <sunpeng93@jd.com>
2020-07-08 18:48:06 +0800

    girl

--- src/main/java/com/example/abstracttest/GirlRunC.java
+++ src/main/java/com/example/abstracttest/GirlRunC.java
@@ -1,5 +1,6 @@
 package com.example.abstracttest;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 /**
@@ -8,10 +9,12 @@ import org.springframework.stereotype.Service;
  * @Author sunpeng93
  * @Date 2020/7/8
  */
+@Slf4j
 @Service("girlRunc")
 public class GirlRunC extends PersonRunC {
     @Override
     public void runT() {
+        log.info("this is girl run");
         System.out.println("this is girl run");
     }
 }
